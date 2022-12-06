8e6563e25757f14143c8164dd65c4663772a2f75
Liam <liamhellman@gmail.com>
2022-12-05 23:07:02 -0500

    Engine not working yet dont see why but tired

--- engine.py
+++ engine.py
@@ -1,35 +1,46 @@
-__doc__ - """
 
-    (0)(1)(2)
-    (3)(4)(5)
-    (6)(7)(8)
 
-"""
 winState = False
+turn = 0
 
 board = [
-    [1,1,1]
-    [1,1,1]
+    [1,1,1],
+    [1,1,1],
     [1,1,1]
 ]
 
 
 #row
-if board[0] == board[1] == board[2]:
-    winState = True
-if board[3] == board[4] == board[5]:
-    winState = True
-if board[6] == board[7] == board[8]:
-    winState = True
-#collumn
-if board[0] == board[3] == board[6]:
-    winState = True
-if board[1] == board[4] == board[7]:
-    winState = True
-if board[2] == board[5] == board[8]:
-    winState = True
-#diagonal
-if board[0] == board[4] == board[8]:
-    winState = True
-if board[2] == board[4] == board[6]:
-    winState = True
+while winState == False or turn <10:
+    turn = turn + 1
+
+    turnX = input("Your xsign ")
+    turnY = input("Your y sign ")
+
+
+    if turn %2 == 0:
+        board[turnX][turnY] == '0'
+    if turn %2 != 0:
+        board[turnX][turnY] == '0'
+
+    print(board)
+
+    #rows
+    if board[0][0] == board[1][0] == board[2][0]:
+        winState = True
+    if board[0][1] == board[1][1] == board[2][1]:
+        winState = True
+    if board[0][2] == board[1][2] == board[2][2]:
+        winState = True
+    #collumn
+    if board[0][0] == board[0][1] == board[0][2]:
+        winState = True
+    if board[1][0] == board[1][1] == board[1][2]:
+        winState = True
+    if board[2][0] == board[2][1] == board[2][2]:
+        winState = True
+    #diagonal
+    if board[0][0] == board[1][1] == board[2][2]:
+        winState = True
+    if board[2][0] == board[1][1] == board[0][2]:
+        winState = True
