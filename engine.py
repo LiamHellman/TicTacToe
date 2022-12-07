b3afc032d934a2aeaeeda5e28ef65c46cdf22bdb
Liam <liamhellman@gmail.com>
2022-12-06 20:44:55 -0500

    Basic engine needs limits to play on same slot

--- engine.py
+++ engine.py
@@ -1,17 +1,15 @@
-
+import numpy as np
 
 winState = False
 turn = 0
-
 board = [
-    [1,1,1],
-    [1,1,1],
-    [1,1,1]
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
 ]
 
 
-#row
-while winState == False or turn <10:
+while winState == False and turn <10:
     turn = turn + 1
 
     turnX = input("Your xsign ")
@@ -19,18 +17,18 @@ while winState == False or turn <10:
 
 
     if turn %2 == 0:
-        board[turnX][turnY] == '0'
+        board[int(turnX)][int(turnY)] = "O" 
     if turn %2 != 0:
-        board[turnX][turnY] == '0'
+        board[int(turnX)][int(turnY)] = "X"
 
-    print(board)
+    print(np.matrix(board))
 
     #rows
-    if board[0][0] == board[1][0] == board[2][0]:
+    if board[0][0] == board[1][0] and board[1][0] == board[2][0]:
         winState = True
-    if board[0][1] == board[1][1] == board[2][1]:
+    if board[0][1] == board[1][1] and board[1][1] == board[2][1]:
         winState = True
-    if board[0][2] == board[1][2] == board[2][2]:
+    if board[0][2] == board[1][2] and board[1][2] == board[2][2]:
         winState = True
     #collumn
     if board[0][0] == board[0][1] == board[0][2]:
@@ -44,3 +42,11 @@ while winState == False or turn <10:
         winState = True
     if board[2][0] == board[1][1] == board[0][2]:
         winState = True
+
+if turn == 10:
+    print("tie")
+elif turn % 2 == 0:
+    print("O Win")
+elif turn % 2 == 1:
+    print("X Win")
+
